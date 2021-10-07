789ab94e854b6e3de41cb14c963201e93f95a7f4
holyandy97 <andrianmoroz97@gmail.com>
2021-10-07 15:48:23 +0300

    update simple task

--- task_hello.py
+++ task_hello.py
@@ -1,1 +1,7 @@
-print('Hello from Andrey')
+import requests
+
+response = requests.get('https://github.com/holyandy97')
+al = response.text
+title = al[al.find('<title>') + 7: al.find('</title>')]
+get_name = title[title.find('(') + 1: title.find(')')]
+print(f'Hello from {get_name}')
