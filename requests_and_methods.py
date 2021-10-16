2661b6d67cd265edac9fa36ae101c51817b22ad7
holyandy97 <andrianmoroz97@gmail.com>
2021-10-17 02:30:17 +0300

    ex. 7 is added

---
+++ requests_and_methods.py
@@ -0,0 +1,25 @@
+import requests
+
+url = "https://playground.learnqa.ru/ajax/api/compare_query_type"
+response = requests.get(url)
+print(f'Ответ 1: {response, response.text}')
+
+response = requests.head(url, data={"method": "HEAD"})
+print(f'Ответ 2: {response, response.text}\nCервер не смог обработать запрос, отправленный клиентом из-за '
+      f'неверного синтаксиса')
+response = requests.post(url, data={"method": "POST"})
+print(f'Ответ 3: {response, response.text}')
+
+methods = ['GET', 'POST', 'PUT', 'DELETE']
+for method in methods:
+    for method_value in methods:
+        if method == 'GET':
+            response = requests.request(method, url, params={'method': method_value})
+            if method != method_value and 'success' in response.text:
+                print(f'Ответ 4:\nНе совпадает реальный тип запроса{method} и параметр {method_value}, '
+                      f'но сервер отвечает {response.text}')
+        else:
+            response = requests.request(method, url, data={'method': method_value})
+            if method != method_value and 'success' in response.text:
+                print(f'Ответ 4:\nНе совпадают реальный тип запроса {method} и параметр {method_value}, '
+                      f'но сервер отвечает {response.text}')
