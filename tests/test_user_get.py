aab4a8ed2550265ba7193ef54088b3a50a589ec7
holyandy97 <andrianmoroz97@gmail.com>
2021-11-07 04:24:08 +0300

    test updated

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -28,14 +28,12 @@ class TestUserGet(BaseCase):
         Assertions.assert_json_has_keys(response2, expected_fields)
 
     def test_get_user_details_auth_as_other_user(self):
-        data = {'email': 'vinkotov@example.com', 'password': '1234'}
-        response1 = MyRequests.post(url="/user/login", data=data)
-        auth_sid = self.get_cookie(response1, "auth_sid")
-        token = self.get_header(response1, "x-csrf-token")
-        user_id_from_auth_method = self.get_json_value(response1, "user_id")
+        data, user_id_1 = self.register()
+        data2, user_id_2 = self.register()
+        response, auth_sid, token = self.login(data['email'], data['password'])
 
-        response2 = MyRequests.get(url=f"/user/{user_id_from_auth_method+self.random_number()}",
-                                   headers={"x-csrf-token": token},
-                                   cookies={"auth_sid": auth_sid})
-        Assertions.assert_json_has_key(response2, 'username')
-        assert len(dict(response2.json())) == 1, f"Unexpected response: JSON has not only 'username'"
+        get_response = MyRequests.get(f'/user/{user_id_2}',
+                                      headers={"x-csrf-token": token},
+                                      cookies={"auth_sid": auth_sid})
+        Assertions.assert_code_status(get_response, 200)
+        assert get_response.json() == {'username': data2['username']}
